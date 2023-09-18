3d16b5e4898f6c36fb9b098b48ac7697ee6e9d0f
zhaoname666 <zhaoname666@users.noreply.github.com>
2023-09-18 14:19:29 +0000

    deploy: ecb773b4ee09768deb4d46abc7184efb595565a9

---
+++ js/3.636de451.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_demo"]=self["webpackChunkvue_demo"]||[]).push([[3],{8003:function(e,u,n){n.r(u),n.d(u,{default:function(){return d}});var c=n(3396),o=n(4870),r=n(2483);const s=["src"];var t={__name:"FormTo",setup(e){const u=(0,r.yj)();console.log(u.query.img);const n=(0,o.iH)(u.query.img);return(e,u)=>((0,c.wg)(),(0,c.iD)("div",null,[(0,c._)("img",{src:n.value,alt:""},null,8,s)]))}},a=n(89);const l=(0,a.Z)(t,[["__scopeId","data-v-152dcad1"]]);var d=l}}]);
+//# sourceMappingURL=3.636de451.js.map
